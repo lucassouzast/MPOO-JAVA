0cf18212c4599fe4c70e65b3f7a617c329e9139f
lucassouzast <45800788+lucassouzast@users.noreply.github.com>
2019-08-19 19:19:01 -0300

    Add files via upload
    Arquivos da aula relacionada a 12/08/2019.

---
+++ Lucas MPOO 12082019/aula1208/src/Fruta.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Fruta {
+		
+	
+	/*
+	 *  O que deve contem uma classe?	
+	 * 
+	 * 
+	 */
+	public String nome;
+	public boolean isClimaterica;
+	
+	public Fruta(String nome, boolean isClimaterica){
+		this.nome = nome;
+		this.isClimaterica = isClimaterica;
+	}
+	//uma vez criado o construtor o default deixa de existir 
+}
